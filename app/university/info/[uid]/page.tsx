23bfdb7162341279908ec882267693b82346756c
NebulaClear <2256611082@qq.com>
2025-03-29 14:05:36 +0800

    fix:uid type bug

--- app/university/info/[uid]/page.tsx
+++ app/university/info/[uid]/page.tsx
@@ -1,20 +1,25 @@
 // src/app/page.tsx
-import UFooter from '@/components/University/UFooter'
-import UBanner from '@/components/University/UBanner'
-import UStandard from '@/components/University/UStandard'
-import UInfo from '@/components/University/UInfo'
-import ULocation from '@/components/University/ULocation'
+import UFooter from "@/components/University/UFooter";
+import UBanner from "@/components/University/UBanner";
+import UStandard from "@/components/University/UStandard";
+import UInfo from "@/components/University/UInfo";
+import ULocation from "@/components/University/ULocation";
 
+interface PageProps {
+  params: Promise<{  
+    uid: string;
+  }>;
+}
 
-export default function UniInfo({ params }: { params: { uid: string | number } }) {
-  const {uid}=params;
+export default async function UniInfo({params}: PageProps) {
+  const { uid } = await params;
   return (
     <main className="relative">
-      <UBanner uid={uid}  />
+      <UBanner uid={uid} />
       <UStandard />
       <UInfo />
       <ULocation />
       <UFooter />
     </main>
-  )
+  );
 }
